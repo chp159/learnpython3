7c50dbd5d46374e643bf6748fbc5d2e22a3af679
ChenPeng-PC <475602529@qq.com>
2016-07-27 20:54:18 +0800

    修改TCPServer和client代码：正常输出字符串！

--- TCPServer.py
+++ TCPServer.py
@@ -8,17 +8,18 @@ from socketserver import TCPServer,StreamRequestHandler
 
 host = socket.gethostbyname(socket.gethostname())
 port = int(input('请输入端口号：'))
-
+BUF_SIZE = 4096
 
 class myhandler(StreamRequestHandler):
     def handle(self):
         while True:
             try:
-                data = self.rfile.readline().strip()
+                data = self.rfile.readline().strip().decode()
+                #data = self.connection.recv(BUF_SIZE)
                 print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    ' +data)
             except ConnectionResetError:
                 #traceback.print_exc()
-                print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+' disconnected!')
+                print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    disconnected!')
                 break
 
 server = TCPServer((host,port),myhandler)
--- client.py
+++ client.py
@@ -5,13 +5,13 @@ import socket
 import time
 from time import strftime,localtime
 
-host = '192.168.4.86'
+host = '192.168.31.127'
 port = 5000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在客户端开启心跳维护
 client.connect((host, port))
 while True:
-    client.send('Hello World!'.encode())
+    client.sendall('Hello World!\n'.encode())
     print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'  '+'Send data!')
     time.sleep(1) #如果想验证长时间没发数据，SOCKET连接会不会断开，则可以设置时间长一点
