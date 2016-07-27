35d7f854e05778646f261c9680e719760075a71d
ChenPeng-PC <475602529@qq.com>
2016-07-27 21:07:48 +0800

    增加部分注释

--- TCPServer.py
+++ TCPServer.py
@@ -5,16 +5,19 @@ import socket
 import traceback
 from time import strftime,localtime
 from socketserver import TCPServer,StreamRequestHandler
+'''
+TCPServer对象实例化参数有两个，一个是host,port的tuple，一个是handler类，把需要做的处理放到handler中。
+'''
+host = socket.gethostbyname(socket.gethostname())   #获取本机IP地址
+port = int(input('请输入端口号：'))    #输入Server端口号
+#BUF_SIZE = 4096
 
-host = socket.gethostbyname(socket.gethostname())
-port = int(input('请输入端口号：'))
-BUF_SIZE = 4096
-
-class myhandler(StreamRequestHandler):
+class myhandler(StreamRequestHandler):      #自定义handler类
     def handle(self):
         while True:
             try:
-                data = self.rfile.readline().strip().decode()
+                data = self.rfile.readline().strip().decode()       #使用rfile接收，无需考虑大小限制；
+                                                                    # decode()函数把bytes类型转化为str
                 #data = self.connection.recv(BUF_SIZE)
                 print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    ' +data)
             except ConnectionResetError:
--- client.py
+++ client.py
@@ -10,8 +10,8 @@ port = 5000
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在客户端开启心跳维护
-client.connect((host, port))
+client.connect((host, port))        #建立连接
 while True:
-    client.sendall('Hello World!\n'.encode())
-    print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'  '+'Send data!')
+    client.sendall('Hello World!\n'.encode())       #encode函数把str转化为bytes
+    print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'  '+'Send data!')      #打印出当前时间
     time.sleep(1) #如果想验证长时间没发数据，SOCKET连接会不会断开，则可以设置时间长一点
