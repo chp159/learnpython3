5ea76d025a7db7c4555aa294801a5e8338b6c34a
chp159 <475602529@qq.com>
2016-08-01 14:49:13 +0800

    修改TCPServer文件并增加TCPClient文件，完善代码，完全自自定义，已初步完成基本代码

---
+++ TCPClient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import socket
+import time
+from time import strftime,localtime
+'''
+TCP客户端脚本，自定义服务器端IP及端口号，并选择是否自动发送，自定义发送时间间隔，自定义发送内容，自动保持心跳维护。
+发送信息成功后接收服务端返回信息并打印。
+Python3添加bytes类型，所以需要把发送信息通过encode()方法把str转化为bytes发送，接收时通过decode()方法把bytes转化为str才能打印。
+'''
+host = input('请输入服务端IP地址：')
+port = int(input('请输入服务端端口地址：'))
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) #在客户端开启心跳维护
+client.connect((host, port))        #建立连接
+
+ias = input('是否自动发送（y/n）：')     #选择是否自动发送信息，y代表Yes，n代表No
+if ias == 'y':
+    t = float(input('请输入发送间隔（单位为秒，毫秒请输入小数，0为无间隔）：'))
+    data = input('请输入要发送的信息：') + '\n'  # 输入需要发送的信息
+    while True:
+        client.sendall(data.encode())       #encode函数把str转化为bytes
+        recvdata = client.recv(409600).decode()
+        print(strftime('%Y-%m-%d %H:%M:%S', localtime()) + '  ' + recvdata)  # 打印出当前时间
+        time.sleep(t) #如果想验证长时间没发数据，SOCKET连接会不会断开，则可以设置时间长一点
+else :
+    while True:
+        data = input('请输入要发送的信息：') + '\n'
+        client.sendall(data.encode())  # encode函数把str转化为bytes
+        recvdata = client.recv(409600).decode()
+        print(strftime('%Y-%m-%d %H:%M:%S', localtime()) + '  ' + recvdata)  # 打印出当前时间
--- TCPServer.py
+++ TCPServer.py
@@ -4,26 +4,34 @@
 import socket
 import traceback
 from time import strftime,localtime
-from socketserver import TCPServer,StreamRequestHandler
+from socketserver import ThreadingTCPServer,StreamRequestHandler
 '''
 TCPServer对象实例化参数有两个，一个是host,port的tuple，一个是handler类，把需要做的处理放到handler中。
+ThreadingTCPServer自动做多线程处理。
+自定义端口号，打印出已连接设备IP地址、端口号及连接时间。
+打印出接收到的信息且回复发送成功信息。
+Python3添加bytes类型，所以需要把发送信息通过encode()方法把str转化为bytes发送，接收时通过decode()方法把bytes转化为str才能打印。
 '''
 host = socket.gethostbyname(socket.gethostname())   #获取本机IP地址
 port = int(input('请输入端口号：'))    #输入Server端口号
-#BUF_SIZE = 4096
 
 class myhandler(StreamRequestHandler):      #自定义handler类
     def handle(self):
+        print(strftime('%Y-%m-%d %H:%M:%S', localtime()) + '   ' + str(self.client_address) + '    已连接!')     #打印出已连接的客户端地址
         while True:
             try:
                 data = self.rfile.readline().strip().decode()       #使用rfile接收，无需考虑大小限制；
                                                                     # decode()函数把bytes类型转化为str
-                #data = self.connection.recv(BUF_SIZE)
-                print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    ' +data)
+                if data:                                            #若不加if else，则客户端断开连接后，仍然会继续打印空数据
+                    print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    ' +data)
+                    self.wfile.write('发送成功！'.encode())
+                else:
+                    break
             except ConnectionResetError:
-                #traceback.print_exc()
-                print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    disconnected!')
+                print(strftime('%Y-%m-%d %H:%M:%S',localtime())+'   '+str(self.client_address)+'    断开连接!')
                 break
+            except:
+                traceback.print_exc()
 
-server = TCPServer((host,port),myhandler)
+server = ThreadingTCPServer((host,port),myhandler)
 server.serve_forever()
